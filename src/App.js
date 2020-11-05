1fed4245cc99811a6dbbebe55f178eff3b4f4593
denimcouch <alexmata92@ymail.com>
2020-11-05 14:33:40 -0600

    created basic file structure

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
-import React from 'react'
+import React, {Component} from 'react'
 import './App.css';
 
-function App() {
-  return (
+class App extends Component{
+
+  state = {
+    villagers: [],
+    fish: [],
+    bugs: []
+  }
+
+  render () {
+    return (
     <div className="App wrapper">
-      <h1>Hello World</h1>
+      <h1 className="acnh-text">Hello World</h1>
     </div>
   );
+  }
+  
+
 }
 
 export default App;
