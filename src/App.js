f6fe39d066784936172b200bb5a7dba3b46b6e89
denimcouch <alexmata92@ymail.com>
2020-11-06 16:02:07 -0600

    all routes linked up

--- src/App.js
+++ src/App.js
@@ -1,22 +1,23 @@
-import React, {Component} from 'react'
-import './App.css';
-import Header from './components/Header'
-import MainContainer from './containers/MainContainer';
+import React, { Component } from "react";
+import "./App.css";
+import Header from "./components/Header";
+import MainContainer from "./containers/MainContainer";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import LandingPage from './containers/LandingPage'
 
-
-class App extends Component{
-
-  
-  render () {
+class App extends Component {
+  render() {
     return (
-    <div className="App wrapper">
-      <Header />
-      <MainContainer/>
-    </div>
-  );
+      <Router>
+        <div className="App wrapper">
+          <Header />
+          <Route exact path="/" component={LandingPage} />
+          <Route exact path='/home' render={routerProps => <MainContainer {...routerProps} />} />
+          
+        </div>
+      </Router>
+    );
   }
-  
-
 }
 
 export default App;
--- src/containers/DisplayContainer.js
+++ src/containers/DisplayContainer.js
@@ -1,24 +1,41 @@
-import React from 'react'
-import UserSideBar from './UserSideBar'
-import {Route} from 'react-router-dom'
-import VillagersList from './VillagersList'
-import BugsList from './BugsList'
-import FishList from './FishList'
-import UserHome from './UserHome'
+import React from "react";
+import UserSideBar from "./UserSideBar";
+import { Route } from "react-router-dom";
+import VillagersList from "./VillagersList";
+import BugsList from "./BugsList";
+import FishList from "./FishList";
+import UserHome from "./UserHome";
 
 const DisplayContainer = (props) => {
+  let { villagers, fish, bugs, user, match } = props;
 
-    let {villagers, fish, bugs, user} = props
+  return (
+    <div id="display-container" className="title friend">
+      <UserSideBar user={user} />
+      <Route
+        exact
+        path={`${match.url}`}
+        render={(routerProps) => <UserHome {...routerProps} user={user} />}
+      />
+      <Route
+        exact
+        path={`${match.url}/villagers`}
+        render={(routerProps) => (
+          <VillagersList {...routerProps} villagers={villagers} />
+        )}
+      />
+      <Route
+        exact
+        path={`${match.url}/bugs`}
+        render={(routerProps) => <BugsList {...routerProps} bugs={bugs} />}
+      />
+      <Route
+        exact
+        path={`${match.url}/fish`}
+        render={(routerProps) => <FishList {...routerProps} fish={fish} />}
+      />
+    </div>
+  );
+};
 
-    return (
-        <div id="display-container" className="title friend">
-            <UserSideBar user={user}/>
-            <Route exact path="/homepage" render={routerProps => <UserHome {...routerProps} user={user} />} />
-            <Route exact path="/villagers" render={routerProps => <VillagersList {...routerProps} villagers={villagers}/>} />
-            <Route exact path="/bugs" render={routerProps => <BugsList {...routerProps} bugs={bugs}/>} />
-            <Route exact path="/fish" render={routerProps => <FishList {...routerProps} fish={fish}/>} />
-        </div>
-    )
-}
-
-export default DisplayContainer
+export default DisplayContainer;
