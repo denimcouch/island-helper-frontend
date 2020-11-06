152e7250d7ee9f4fe4120d87538690d791bc519b
denimcouch <alexmata92@ymail.com>
2020-11-06 13:38:41 -0600

    lil file cleanup

--- src/containers/UserSideBar.js
+++ src/containers/UserSideBar.js
@@ -6,7 +6,7 @@ const UserSideBar = (props) => {
         <div id='user-sidebar' className="section teal">
             <h1 className="acnh-text">{user.name}</h1>
             <h3>Island: {user.town_name}</h3>
-            <h3>Hemisphere: {user.hemisphere.charAt(0).toUpperCase() + user.hemisphere.slice(1)}</h3>
+            <h3>Hemisphere: {user.hemisphere}</h3>
         </div>
     )
 }
--- src/containers/VillagersList.js
+++ src/containers/VillagersList.js
@@ -1,4 +1,4 @@
-import React, { Fragment } from "react";
+import React from "react";
 import Villager from "../components/Villager";
 
 const VillagersList = (props) => {
